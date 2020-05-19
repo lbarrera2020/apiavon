c52d8b3092094ea82cf8cd6822442cf42d6d64d0
Luis Barrera <lbarrera@salud.gob.sv>
2020-05-19 10:28:09 -0600

    se crea metodo categoria

---
+++ src/Controller/DatosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorias;
+use App\Entity\User;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Doctrine\DBAL\Driver\Connection;
+use Symfony\Component\Security\Core\Security;
+use App\Service\JsonSchema;
+use JsonSchema\Validator;
+
+/**
+ * Class DatosController
+ *
+ * @Route("/")
+ */
+class DatosController extends AbstractFOSRestController
+{
+    private $serializer;
+    private $kernel;
+    private $connection;
+    private $security;
+
+    public function __construct(SerializerInterface $serializer, KernelInterface $kernel, Connection $connection, Security $security)
+    {
+        $this->serializer = $serializer;
+        $this->kernel = $kernel;
+        $this->connection = $connection;
+        $this->security = $security;
+    }
+
+    /**
+     * @Rest\Post("/categorias", name="categorias")
+     *
+     */
+    public function getLibros(Request $request)
+    {
+        // Inicialización de variables
+        $em = $this->em;
+        $serializer = $this->serializer;
+        $datos = '[]';
+        $result = $em->getRepository(Categorias::class)->findAll();
+
+        if ($result) {
+            // Convirtiendo el resultado de objetos a jsons
+            $datos = $serializer->serialize($result, 'json');
+        }
+
+
+// Retornando response
+        return new JsonResponse($datos, JsonResponse::HTTP_OK, array(), true);
+
+
+    }
+}
